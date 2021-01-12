d6820676e51e1abeab34ec14efa95d4b23525469
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-12 03:20:30 +0200

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar";
 import BookContextProvider from "./contexts/BookContext";
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
-        
+        <Navbar />
       </BookContextProvider>
     </div> 
   );
