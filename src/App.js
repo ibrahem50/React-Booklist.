94798c3bc7459e15157d05921e01e2ca1c0d7639
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-12 04:03:45 +0200

    added bookform component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import BookForm from "./components/BookForm";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import BookContextProvider from "./contexts/BookContext";
@@ -7,7 +8,8 @@ function App() {
     <div className="App">
       <BookContextProvider>
         <Navbar />
-        <BookList/>
+        <BookList />
+        <BookForm />
       </BookContextProvider>
       <p>Click on book to delete..</p>
     </div> 
