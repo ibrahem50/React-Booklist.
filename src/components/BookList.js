efbc0ed57dcddc4b87bc62e52abdcada33268c1b
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-12 03:35:42 +0200

    added BookList component

---
+++ src/components/BookList.js
@@ -0,0 +1,9 @@
+import React,{useContext} from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+const BookList = () => {
+    const {books}=useContext(BookContext)
+    return (  );
+}
+ 
+export default BookList;
